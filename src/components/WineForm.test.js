0f19447b14a21b65f09367f65314612fefc49c73
Emil Folino <emilfolino@gmail.com>
2022-09-15 14:46:24 +0200

    Maybe fixed test

--- src/components/WineForm.test.js
+++ src/components/WineForm.test.js
@@ -1,2 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import WineForm from './WineForm';
+
+const faker = jest.fn();
+
+test('renders Faustino wines', () => {
+    render(<WineForm submitFunction={faker} />);
+    const label = screen.getByText(/namn:/i);
+    expect(label).toBeInTheDocument();
+});
